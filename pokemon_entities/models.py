72e1da7dbfc7d8688f42030ed3af58d9254aeb85
Vladus0 <118992340+Vladus0@users.noreply.github.com>
2026-02-11 18:20:30 +0400

    show Pokemon name in admin UI.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,4 +2,11 @@ from django.db import models  # noqa F401
 
 # your models here
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200, blank=True)
+    title = models.CharField(max_length=200, verbose_name="Название покемона")
+
+    def __str__(self):
+        return self.title
+
+
+class PokemonEntities(models.Model):
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name='entites', verbose_name="Ссылка на покемона")
