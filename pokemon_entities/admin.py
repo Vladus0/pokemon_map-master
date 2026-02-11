be8aa9b772e18a11df15435d479d155dab1c51ae
Vladus0 <118992340+Vladus0@users.noreply.github.com>
2026-02-11 18:56:50 +0400

    add Pokemon.image model field

--- pokemon_entities/admin.py
+++ pokemon_entities/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Pokemon
+from .models import Pokemon, PokemonEntities
 
 
 admin.site.register(Pokemon)
 
+admin.site.register(PokemonEntities)
--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,6 +3,7 @@ from django.db import models  # noqa F401
 # your models here
 class Pokemon(models.Model):
     title = models.CharField(max_length=200, verbose_name="Название покемона")
+    image = models.ImageField(upload_to="image/bulbazavr.png", verbose_name="Ваше изображение", null=True)
 
     def __str__(self):
         return self.title
@@ -10,3 +11,5 @@ class Pokemon(models.Model):
 
 class PokemonEntities(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name='entites', verbose_name="Ссылка на покемона")
+    # lat = models.FloatField()
+    # lon = models.FloatField()
